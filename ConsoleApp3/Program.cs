168e09c38299724202a143f653d03c3a71470bb2
HidanYugakure <fidants@code.edu.az>
2023-10-13 20:49:00 +0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,95 @@
+﻿#region task 
+// 4 cü tapşırıq mərtəbələri tap
+//int num = 177;
+//int outcome = 0;
+//while (num > 0)
+//{
+//    outcome++;
+//    num /= 10;
+//}
+//Console.WriteLine(outcome);
+#endregion
+#region task  
+//// 1 ci tapsiriq
+//int first_num = 2;
+//int end_num = 24;
+//int counter = 0;
+//for (int i = first_num; i <= end_num; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        counter++;
+//    }
+//}
+//Console.WriteLine(counter);
+#endregion
+#region task 
+// 3 cu tapsiriq;
+
+///int num = 15;
+//if (num % 2 == 0)
+//{
+//    num = num / 2;
+//}
+//else
+//{
+//    if (num == 0) ;
+//    {
+//        Console.WriteLine(true);
+//    }
+
+//    {
+//        Console.WriteLine(false);
+//    }
+//}
+
+#endregion
+#region task
+//n 2 ci tapsiriq tam ədədinin sadə və ya mürəkkəb ədəd olduğunu tapın.
+
+//int num_a = 24;
+//if (num_a == 1)
+//{
+//    Console.WriteLine($"(num_a) bu sade ve murekkebdir"); ;
+//}
+//else if (num_a == 2 || num_a == 3)
+//{
+//    Console.WriteLine($"(num_a) sade ededdir");
+//}
+//else if (num_a % 2 == 0 || num_a % 3 == 0)
+//{
+//    Console.WriteLine($"(num_a) murekkebdir ededdir");
+//}
+//else
+//{
+//    Console.WriteLine($"(num_a) sade ededdir");
+//}
+
+#endregion
+
+#region
+
+//int numa = 16;
+//int count = 0;
+
+//while(numa % 2 == 0)
+//{
+//    if(numa>=1  && numa %2 ==0)
+//    {
+//        numa /= 2;
+//        count++;
+
+        
+//    }
+//}
+//if (count > 1)
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
+
+#endregion
+
